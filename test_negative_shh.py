03d1ce731d4312324f92f3783cbb3e566a142c39
Александр Доронин <107746307+alexdoronin5883@users.noreply.github.com>
2023-09-18 16:19:11 +0300

    Create test_negative_shh.py

---
+++ test_negative_shh.py
@@ -0,0 +1,31 @@
+from sshcheckers import ssh_checkout_negative, ssh_checkout
+import yaml
+
+
+with open('config.yaml') as f:
+   data = yaml.safe_load(f)
+
+
+class Testneg:
+   def test_nstep1(self, make_folders, make_bad_arx):
+       assert ssh_checkout_negative(data["ip"], data["user"], data["passwd"], "cd {}; 7z e {}.{} -o{} -y".format(data["folder_out"], make_bad_arx, data["type"],
+                                                                       data["folder_ext"]), "ERROR:", ), "test1 FAIL"
+
+
+   def test_nstep2(self, make_bad_arx):
+
+       assert ssh_checkout_negative(data["ip"], data["user"], data["passwd"], "cd {}; 7z t {}.{}".format(data["folder_out"], make_bad_arx,
+                                                               data["type"]), "ERROR:"), "test2 FAIL"
+
+
+   def test_nstep3(self):
+       res = []
+       res.append(ssh_checkout(data["ip"], data["user"], data["passwd"], "echo {} | sudo -S dpkg -r"
+                                                                         " {}".format(data["passwd"], data["pkgname"]),
+                               "Удаляется", True))
+       res.append(ssh_checkout(data["ip"], data["user"], data["passwd"], "echo {} | "
+                                                                         "sudo -S dpkg -s {}".format(data["passwd"],
+                                                                                                     data["pkgname"]),
+                               "Status: deinstall ok", True))
+       assert all(res), "test3 FAIL"
+
